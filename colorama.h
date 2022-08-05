66af6401cb5468bf14f408f934a71f09d8f5db9e
Creep416 <Creep@MainPC>
2022-08-05 07:53:27 +0300

    add colorama.h for change terminal color

---
+++ colorama.h
@@ -0,0 +1,29 @@
+#ifndef COLORAMA_H
+#define COLORAMA_H
+#include <iostream>
+#include <windows.h>
+using namespace std;
+enum ConsoleColor
+{
+	Black		=0,
+	Blue		=1,
+	Green		=2,
+	Cyan		=3,
+	Red			=4,
+	Magenta		=5,
+	Brown		=6,
+	LightGray	=7,
+	DarkGray	=8,
+	LightBlue	=9,
+	LightGreen	=10,
+	LightCyan	=11,
+	LightRed	=12,
+	LightMagenta=13,
+	Yellow		=14,
+	White		=15
+};
+void SetColor(int text, int background)
+{
+	
+}
+#endif COLORAMA_H
